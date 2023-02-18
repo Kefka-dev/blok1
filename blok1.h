43e636b6f7d26ee5aab1688ca9a67c03f92e2930
Kefka <50819728+Kefka-dev@users.noreply.github.com>
2023-02-18 21:36:59 +0100

    splnena uloha1_2(arabToRoman)
    vytvorena funkcia arabToRoman, funkcia pocita od 1-100, osetrenie vstupu nebolo v zadani

--- blok1.h
+++ blok1.h
@@ -3,4 +3,5 @@
 
 int uloha1_1(int cislo, int asciiKod);
 int decToBin(int cislo);
+int arabToRoman(int cislo);
 #endif
--- main.c
+++ main.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-    int cislo, asciiKod;
+    int cislo, asciiKod, vstupUzivatela;
     cislo = 33777;
     asciiKod = 88;
-    uloha1_1(cislo, asciiKod);
+    //uloha1_1(cislo, asciiKod);
+    scanf("%d", &vstupUzivatela);
+    arabToRoman(vstupUzivatela);
     return 0;
 }
 
@@ -60,4 +62,58 @@ int decToBin(int cislo)
         }
         return 0;
     }
+}
+
+int arabToRoman(int cislo)
+{
+    while (cislo>0)
+    {
+        if (cislo==100)
+        {
+            printf("C");
+            cislo = cislo - 100;
+        }
+        else if(cislo>=90)
+        {
+            printf("XC");
+            cislo = cislo - 90;
+        }
+        else if (cislo>=50)
+        {
+            printf("L");
+            cislo = cislo - 50;
+        }
+        else if (cislo>=40)
+        {
+            printf("XL");
+            cislo = cislo - 40;
+        }
+        else if (cislo>=10)
+        {
+            printf("X");
+            cislo = cislo - 10;
+        }
+        else if (cislo == 9)
+        {
+            printf("IX");
+            cislo = cislo - 9;
+        }
+        else if (cislo>=5)
+        {
+            printf("V");
+            cislo = cislo - 5;
+        }
+        else if (cislo == 4)
+        {
+            printf("IV");
+            cislo = cislo - 4;
+        }
+        else if (cislo >=1)
+        {
+            printf("I");
+            cislo = cislo - 1;
+        }
+        //printf("\ncislo=%d\n", cislo);
+    }    
+    return 0;
 }
